eff5f011231f0fabc01857f75bd074f68088a3d7
qitao.yang <qitaoyang.yqt@alibaba-inc.com>
2021-03-22 14:07:04 +0800

    Initial Commit

---
+++ SDThumbnailDecodingPeakMemoryTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SDThumbnailDecodingPeakMemoryTest
+//
+//  Created by eleme-ky on 2021/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SDThumbnailDecodingPeakMemoryTest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  SDThumbnailDecodingPeakMemoryTest
+//
+//  Created by eleme-ky on 2021/3/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
